532f9a0b6b4ef61a9b2047fdbe313962b3819e11
Alexander Liman <liman324@yandex.ru>
2019-07-22 09:57:45 +0600

    Create M62429.ino

---
+++ M62429.ino
@@ -0,0 +1,19 @@
+#include <M62429.h>
+
+void setup(){
+}
+
+void loop(){
+audio();
+delay(1000);
+}
+
+void audio(){
+setVolume (11,12,0,0,5); 
+setVolume (11,12,0,1,5);
+/* pin CLK
+   pin DATA
+   0 - по одному каналу, 1 - оба вместе
+   0 ПК 1 ЛК
+   83 ... 0 горомкость 83 = -83 дБ */  
+}
