ce7f192d8c49acd6d4ea9ec6aa98fc0775bd1c8f
Alexander Liman <liman324@yandex.ru>
2019-07-22 09:55:58 +0600

    Create M62429.h

---
+++ M62429.h
@@ -0,0 +1,8 @@
+//rcl-radio.ru
+// liman324@yandex.ru
+// библиотека создана на основе https://soltau.ru/index.php/arduino/item/497-kak-podklyuchit-regulyator-gromkosti-m62429-k-arduino
+
+#include <Arduino.h>
+
+void setVolume (int clk, int data, int canal, int k, int att);
+	
